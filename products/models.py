118a8ac4bb3e828e4d3f95a30818f474224714af
cnridley <cnridley1@gmail.com>
2020-10-27 17:29:59 +0000

    made products page and linked to admin etc.

--- products/models.py
+++ products/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 class Product(models.Model):
-    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_url = models.ImageField(upload_to='media')
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    """A view to show all products selling"""
+    product = Product.objects.all()
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products.html', context)
