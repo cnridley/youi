ff7ffe9e9bc2532999afe0e71c5dcd4c2e9c897e
cnridley <cnridley1@gmail.com>
2020-11-06 03:47:27 +0000

    added size selector on merch page

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    has_size =models.BooleanField(default=True, blank=True, null=True)
 
     def __str__(self):
         return self.name
