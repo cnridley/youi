c04ad006e296e82825c3a0272ff80e8a4aee6b95
cnridley <cnridley1@gmail.com>
2020-11-14 17:10:35 +0000

    added checkout bag and form with crispy-form

---
+++ checkout/signals.py
@@ -0,0 +1,30 @@
+"""This is the way to update the order total, delivery cost, and grand_total for each order
+as users add line items to it.
+The basic process is that will first create an order. Then iterate through the shopping bag.
+And add line items to it one by one updating the various costs along the way.
+The method to update the total is already in the order model.
+We just need a way to call it each time a line item is attached to the order.
+To accomplish this we'll use a built-in feature of django called signals.
+"""
+
+from django.db.models.signals import post_save, post_delete
+""" post means after. So this implies these signals are sent by django to the entire application
+after a model instance is saved and after it's deleted respectively."""
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+"""receiver decorator to tell django we are recieving post_save signals frrom the OrderLineItem model"""
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    instance.order.update_total()
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.update_total()
--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
 
 def checkout(request):
-    return render(request, 'checkout.html', context)
+    """retrieve bag from current session"""
+    bag = request.session.get('bag', {})
+    """if nothing in the bag return an error message and re-direct to products page"""
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    """create an instance of order form"""
+    order_form = OrderForm()
+    template = 'checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
