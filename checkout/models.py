9576ab10b259d049fcf19fc5190711800d4cce25
cnridley <cnridley1@gmail.com>
2020-11-19 01:38:56 +0000

    finished checkout page

--- checkout/models.py
+++ checkout/models.py
@@ -1,38 +1,34 @@
-"""used to generate the order number"""
-import uuid 
-
+import uuid
 
 from django.db import models
-"""built in django model to help calculate costs"""
-from django.db.models import Sum 
-"""settings.py file"""
-from django.conf import settings 
-"""OrderLineItem has foreign key to it so need to import it"""
-from products.models import Product 
+from django.db.models import Sum
+from django.conf import settings
 
-""" Order model will handle all orders accross the store"""
+from products.models import Product
+
+
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     country = models.CharField(max_length=40, null=False, blank=False)
-    """ null=true because not every country has postcode"""
-    postcode = models.CharField(max_length=20, null=True, blank=True) 
+    postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    """ null=true because not every country has county"""
-    county = models.CharField(max_length=80, null=True, blank=True) 
-    """ auto_now_add will automatically set date and time """
-    date = models.DateTimeField(auto_now_add=True) 
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
-    def _generate_order_number(self): 
-        """starts with an underscore to tell django that it is a private method that will
-        only be used inside this class. It will generate a random, unique order number using UUID"""
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
@@ -41,9 +37,7 @@ class Order(models.Model):
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
-        """using the sum function across all the line-item total fields for all line items on this order."""
-        """taken from the settings file to calculate delivery"""
-        if self.order_total < settings.FREE_DELIVERY_THRESHOLD: 
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
@@ -62,13 +56,7 @@ class Order(models.Model):
     def __str__(self):
         return self.order_number
 
-""" related to the Order model. A line item will be like an individual shopping bag related to a specific order
-and referencing the product itself.
-Idea here is that when the user checkouts, the information they put in the order from will be used to create an order 
-instance. And then we'll iterate through the items in the shopping bag.
-Creating an order line item for each one. Attaching it to the order.
-And updating the delivery cost, order total, and grand total along the way.
-"""
+
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
