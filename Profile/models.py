93c57a8a64d495608b53eb85265a137be815cdf4
cnridley <cnridley1@gmail.com>
2020-11-28 15:21:14 +0000

    fixed profile model

--- Profile/models.py
+++ Profile/models.py
@@ -17,4 +17,14 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        Profile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.profile.save()
+
 
--- Profile/views.py
+++ Profile/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from .models import Profile
 
 def profile(request):
     """ Display the user's profile. """
-
-    template = 'Profiles.html'
-    context = {}
+    user = Profile.objects.all()
+    template = 'Profile.html'
+    context = {
+        'user': user
+    }
 
     return render(request, template, context)
