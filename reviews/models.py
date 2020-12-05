3cbe8af8be567b4cb54faf844b29a31b7d9761f1
cnridley <cnridley1@gmail.com>
2020-12-05 15:33:31 +0000

    made reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Reviews(models.Model):
+    My_Rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+    name = models.CharField(max_length=50, blank=True, null=True)
+    title = models.CharField(max_length=500, null=True, blank=True)
+    rating = models.IntegerField(choices=My_Rating, null=True, blank=True, default=3)
+    text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
