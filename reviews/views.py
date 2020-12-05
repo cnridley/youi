c2ff7a172d98965f45a0a6148907b6999fab9d4d
cnridley <cnridley1@gmail.com>
2020-12-05 15:59:34 +0000

    styled review page

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .models import Reviews
+from Profile.models import Profile
 
 # Create your views here.
 
 def reviews(request):
+    review = Reviews.objects.all()
+    user = Profile.objects.all()
 
-    context = {}
+    context = {
+        'review' : review,
+        'user': user,
+    }
 
     return render(request, 'reviews.html', context)
