8839383e73f12a09a585f73bd695fca2d61f5fc8
cnridley <cnridley1@gmail.com>
2020-12-10 15:45:04 +0000

    added edit button to progress pictures

--- progression/urls.py
+++ progression/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.progression_pictures, name='progression_pictures'),
+    path('delete/<int:progressionpicture_id>/', views.delete_picture, name='delete_picture'),
+    path('edit/<int:progression_id>/', views.edit_picture, name='edit_picture'),
 ]
--- progression/views.py
+++ progression/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from .models import ProgressionPicture
 from Profile.models import Profile
 from .forms import ProgressionForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -24,3 +26,43 @@ def progression_pictures(request):
     }
 
     return render(request, 'progression_pics.html', context)
+
+@login_required
+def delete_picture(request, progressionpicture_id):
+    """ Delete a product from the store """
+    if not request.user.is_authenticated:
+        messages.info(request, 'Sorry, you can not do that!')
+        return redirect(reverse('Profile'))
+
+    picture = get_object_or_404(ProgressionPicture, pk=progressionpicture_id)
+    picture.delete()
+    messages.info(request, 'Picture deleted!')
+    return redirect(reverse('progression_pictures'))
+
+@login_required
+def edit_picture(request, progression_id):
+    """ Edit a product in the store """
+    if not request.user.is_authenticated:
+        messages.info(request, 'Sorry, you can not do that.')
+        return redirect(reverse('Profile'))
+
+    progression = get_object_or_404(ProgressionPicture, pk=progression_id)
+    if request.method == 'POST':
+        form = ProgressionForm(request.POST, request.FILES, instance=progression)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Successfully updated picture!')
+            return redirect(reverse('progression_pictures'))
+        else:
+            messages.error(request, 'Failed to update picture. Please ensure the form is valid.')
+    else:
+        form = ProgressionForm(instance=progression)
+
+    template = 'edit_picture.html'
+    
+    context = {
+        'form': form,
+        'progression': progression,
+    }
+
+    return render(request, template, context)
