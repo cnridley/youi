04f050a0bc01fb5a18a41078d8ea0c25a439387b
cnridley <cnridley1@gmail.com>
2020-12-10 01:15:54 +0000

    added product form

---
+++ products/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    class meta():
+        model = Product 
+        fields = ('__all__')
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
 from .models import Product
 from Profile.models import Profile
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -9,9 +10,19 @@ def all_products(request):
     products = Product.objects.all()
     user = Profile.objects.all()
 
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('all_products'))
+        
+    else:
+        form = ProductForm()
+
     context = {
         'products': products,
-        'user': user
+        'user': user,
+        'form': form,
     }
 
     return render(request, 'products.html', context)
