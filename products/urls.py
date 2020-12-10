872f41f489110047cad3afef39cb07ebfe68ca7b
cnridley <cnridley1@gmail.com>
2020-12-10 04:04:28 +0000

    added edit and delete to products for superuser

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,8 +3,10 @@ from .models import Product
 from Profile.models import Profile
 from .forms import ProductForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
 
 def all_products(request):
     """A view to show all products selling"""
@@ -28,8 +30,13 @@ def all_products(request):
 
     return render(request, 'products.html', context)
 
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.info(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -50,3 +57,15 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+@login_required
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.info(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
